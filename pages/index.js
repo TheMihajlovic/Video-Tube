a77d56ab621ac89fe08eb209b692a25cc6195907
Igor <hitjaa@gmail.com>
2019-02-08 16:05:30 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default () => (
+    <pre>
+        <div className="alert alert-primary">
+            <p>asdasd</p>
+        </div>
+  </pre>
+)
